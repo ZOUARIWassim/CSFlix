eb365773088ea3e556d1aadc35ac209b838c1043
ZOUARIWassim <wassim.zouari@student-cs.fr>
2025-02-08 22:24:42 +0100

    Add Movie fetch

--- Backend/src/Routes/Movies.js
+++ Backend/src/Routes/Movies.js
@@ -5,14 +5,16 @@ router.get('/getMovies', async (req, res) => {
     response = [];
     for (let page_number = 1; page_number <= 5; page_number++) {
         const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page_number}`;
+        const TMDB_API_KEY = process.env.TMDB_API_KEY;
         const options = {
-            method : 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo'
-            }
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: TMDB_API_KEY
+        }
         };
-        const page_response = await fetch(url, options);
+
+        const page_response = await fetch(url, options)
 
         if (page_response.ok) {
             const page_data = await page_response.json();
