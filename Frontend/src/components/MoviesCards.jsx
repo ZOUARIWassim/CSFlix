19784aced181a1935afe3124e243dab611620133
ZOUARIWassim <wassim.zouari@student-cs.fr>
2025-02-09 19:53:51 +0100

    Add rating fonct.

--- Frontend/src/components/MoviesCards.jsx
+++ Frontend/src/components/MoviesCards.jsx
@@ -6,6 +6,7 @@ import { FaStar } from "react-icons/fa";
 function MoviesCards({ querySerach, listTitle }) {
     const [movies, setMovies] = useState([]);
     const [filteredMovies, setFilteredMovies] = useState([]);
+    const [recommendedMovies, setRecommendedMovies] = useState([]);
 
     useEffect(() => {
         const BackendServer = import.meta.env.VITE_BackendServer;
@@ -17,7 +18,7 @@ function MoviesCards({ querySerach, listTitle }) {
             } catch (error) {
                 console.error("Error fetching movies:", error);
             }
-        }
+        };
 
         getMovies().then((data) => {
             setMovies(data);
@@ -43,11 +44,35 @@ function MoviesCards({ querySerach, listTitle }) {
 
     const handleRate = (movieID,rating) => {
         for (let i = 1; i <= rating; i++) {
-            document.getElementById(`Rate${movieID}-${i}`).style.color = "#f39c12";
+            document.getElementById(`Rate${movieID}-${i}`).style.color = "#cd8107";
         }
         for (let i = rating + 1; i <= 5; i++) {
             document.getElementById(`Rate${movieID}-${i}`).style.color = "#f1c40f";
         }
+        const BackendServer = import.meta.env.VITE_BackendServer;
+        const rateMovie = async () => {
+            try {
+                const response = await fetch(BackendServer + '/user/auth/rate', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        movieId: movieID,
+                        rating: rating,
+                    }),
+                    credentials: 'include',
+                });
+                const data = await response.json();
+                return data;
+            } catch (error) {
+                console.error("Error rating movie:", error);
+            }
+        };
+
+        rateMovie().then((data) => {
+            console.log(data);
+        });
     }
 
     return (
@@ -56,7 +81,7 @@ function MoviesCards({ querySerach, listTitle }) {
                 <p>{listTitle}</p>
             </div>
             <div className="MoviesGrid">
-                {filteredMovies.map((movie) => (
+                {listTitle === "All Movies" && filteredMovies.map((movie) => (
                     <div key={movie.id} className="MovieCard">
                         <div className="MovieImg">
                             <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={"No Poster is Available"} />
@@ -64,7 +89,6 @@ function MoviesCards({ querySerach, listTitle }) {
                         <div className="MovieInfo">
                             <p>{movie.title}</p>
                             <p>{movie.release_date}</p>
-                            {listTitle === "All Movies" && 
                                 <div className="MovieRating">
                                     {[...Array(5)].map((_, index) => (
                                         <FaStar
@@ -74,7 +98,17 @@ function MoviesCards({ querySerach, listTitle }) {
                                         />
                                     ))}
                                 </div>
-                            }
+                        </div>
+                    </div>
+                ))}
+                {listTitle === "Recommended For You" && recommendedMovies.map((movie) => (
+                    <div key={movie.id} className="MovieCard">
+                        <div className="MovieImg">
+                            <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={"No Poster is Available"} />
+                        </div>
+                        <div className="MovieInfo">
+                            <p>{movie.title}</p>
+                            <p>{movie.release_date}</p>
                         </div>
                     </div>
                 ))}
